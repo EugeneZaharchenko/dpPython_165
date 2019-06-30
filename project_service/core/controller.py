3b96f6ed019d808dcbf5207348e4d3a4e8a039e8
pc <eugenezaharchenko@gmail.com>
2019-06-30 23:01:08 +0300

    first commit to group

--- project_service/core/controller.py
+++ project_service/core/controller.py
@@ -1,10 +1,16 @@
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from flask_restful import Resource
-import datetime
 import uuid
+import logging as log
 
+from .utils.schemas import ProjectSchema, DataSchema
 from .utils.parsers import status_parser, data_parser
 from .models import Projects, Data, db
+from .utils.session import session
+
+
+project_schema = ProjectSchema()
+data_schema = DataSchema()
 
 
 # /api/projects
@@ -58,3 +64,69 @@ class ProjectsDataHandler(Resource):
             db.session.add(data_about_room)
         db.session.commit()
         return jsonify(dict(status='write_all'))
+
+
+# /projects/<id>/calc
+class ProjectsCalc(Resource):
+
+    def get(self, id):
+
+        """
+        Method to fetch data of the particular project for calculation
+        :param id: an id of the project
+        """
+        log.debug("GET method")
+        _project = Projects.query.filter_by(id=uuid.UUID(id)).first()
+        if not _project:
+            # abort(404)
+            return {"message": "There is no such project"}, 404
+
+        _id = str(_project.id)
+        _data = Data.query.filter_by(id=uuid.UUID(_id))
+        if not _data:
+            abort(400)
+            return {"message": "No input data provided"}, 400
+        _project.status = "calculation"
+        db.session.commit()
+        output_prj = project_schema.dump(_project).data
+        output_data = data_schema.dump(_data).data
+        return jsonify({"project": output_prj, "data": output_data})
+
+    def post(self, id):
+        """
+        Method to retrieve  calculated data of the particular project
+        :param id: an id of the project
+        """
+        log.debug("POST method")
+
+        # deserialize input json
+        entry_data = request.get_json()
+        if not entry_data:
+            return {"message": "No input data provided"}, 400
+        result = entry_data["result"]
+        return {"result": result}, 200
+
+
+# /api/calc/status/<id>
+class ProjectsCalcResult(Resource):
+    def put(self, id):
+        """
+        Method to update project status data which are in calculation progress
+        :param id: an id of the project
+        """
+        log.debug("PUT method")
+
+        # deserialize input json
+        json_data = request.get_json()
+        if not json_data:
+            return {"msg": "No input data provided"}, 400
+
+        new_status = json_data["status"]
+
+        log.debug(new_status)
+        project = Projects.query.filter_by(id=uuid.UUID(id)).first()
+        if not project:
+            return {"msg": "Can't update - no such project"}, 404
+        project.status = new_status
+        db.session.commit()
+        return {"msg": "Status succefully updated for {}".format(new_status)}, 200
